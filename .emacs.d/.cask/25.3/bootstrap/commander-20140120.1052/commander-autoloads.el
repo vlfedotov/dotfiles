4de519eb45798478ee18050fdff1d9547e832f30
vlfedotov <fedotov@gmail.com>
2017-10-08 13:25:42 +0300

    del exhasting files

--- .emacs.d/.cask/25.3/bootstrap/commander-20140120.1052/commander-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; commander-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("commander.el") (22987 60942 462124 334000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; commander-autoloads.el ends here
--- .emacs.d/.cask/25.3/elpa/with-editor-20170925.528/with-editor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; with-editor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22987 64078 953322 743000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; with-editor-autoloads.el ends here
